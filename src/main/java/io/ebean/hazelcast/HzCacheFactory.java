f3857bf7af448af07eb131f28639c1adaa5a12f8
Alexander Wagner <alexander.wagner@foconis.de>
2023-10-09 16:53:05 +0200

    Fix: put sends invalidation

--- src/main/java/io/ebean/hazelcast/HzCacheFactory.java
+++ src/main/java/io/ebean/hazelcast/HzCacheFactory.java
@@ -190,6 +190,12 @@ public final class HzCacheFactory implements ServerCacheFactory {
     }
 
     @Override
+    public void put(Object key, Object value) {
+      super.put(key, value);
+      sendInvalidation(name);
+    }
+
+    @Override
     public void invalidate() {
       super.clear();
     }
